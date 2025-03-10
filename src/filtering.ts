d4261bcbf63b068c9d59d2ca2bdd810d063cd24f
AbdulrahmanAlshatti <abdulrahmanalshatti0@gmail.com>
2025-03-10 13:32:42 +0300

    Task Done - Abdulrahman Alshatti and Mishari Aldoub

--- src/filtering.ts
+++ src/filtering.ts
@@ -11,8 +11,7 @@ const numbers = [10, 13, 20, 25, 38, 35, 40];
 // greaterThanTwentyFive(numbers); // => [38, 35, 40]
 function greaterThanTwentyFive(numbers: number[]): number[] {
   // write your code here...
-
-  return []; // replace empty array with what you see is fit
+  return numbers.filter(number => number >= 25)
 }
 
 // `divisibleByFive` function that:
@@ -22,8 +21,7 @@ function greaterThanTwentyFive(numbers: number[]): number[] {
 // divisibleByFive(numbers); // => [10, 20, 25, 35, 40]
 function divisibleByFive(numbers: number[]): number[] {
   // write your code here...
-
-  return []; // replace empty array with what you see is fit
+  return numbers.filter(number => number % 5 == 0)
 }
 
 export { greaterThanTwentyFive, divisibleByFive };
