7d3faded0a2adc1db286789530669b8b9584bf41
AK-macbook <aalkhareji27@gmail.com>
2025-03-10 13:13:37 +0300

    Implement iteration methods using map and filter

--- src/filtering.ts
+++ src/filtering.ts
@@ -12,7 +12,7 @@ const numbers = [10, 13, 20, 25, 38, 35, 40];
 function greaterThanTwentyFive(numbers: number[]): number[] {
   // write your code here...
 
-  return []; // replace empty array with what you see is fit
+  return numbers.filter((number) => number >= 25); // replace empty array with what you see is fit
 }
 
 // `divisibleByFive` function that:
@@ -22,8 +22,7 @@ function greaterThanTwentyFive(numbers: number[]): number[] {
 // divisibleByFive(numbers); // => [10, 20, 25, 35, 40]
 function divisibleByFive(numbers: number[]): number[] {
   // write your code here...
-
-  return []; // replace empty array with what you see is fit
+  return numbers.filter((number) => number % 5 == 0); // replace empty array with what you see is fit
 }
 
 export { greaterThanTwentyFive, divisibleByFive };
